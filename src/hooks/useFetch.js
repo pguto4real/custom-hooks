becb9f0b95f395c758b4e64307fa9bfd590b5236
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-02 17:36:20 -0600

    use th custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
-function useFetch(){
-    useEffect(() => {
-        async function fetchPlaces() {
-          setIsFetching(true);
-          try {
-            const places = await fetchUserPlaces();
-            setUserPlaces(places);
-          } catch (error) {
-            setError({ message: error.message || 'Failed to fetch user places.' });
-          }
-    
-          setIsFetching(false);
-        }
-    
-        fetchPlaces();
-      }, []);
-    
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const places = await fetchFn();
+        setFetchedData(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    fetchedData,
+    error,
+  };
 }
